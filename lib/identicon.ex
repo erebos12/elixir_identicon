bebcf63e050b5161764327b0d49862d802c64ae7
Alex Sahm <sahm.alexander@pwc.com>
2019-07-29 09:02:34 +0200

    initial commit for identicon

---
+++ lib/identicon.ex
@@ -0,0 +1,31 @@
+defmodule Identicon do
+   def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+   end
+
+   def build_grid(%Identicon.Image{hex: hex} = image) do
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> mirror_row
+   end
+
+   def mirror_row(row) do
+      # [145, 56, 200]
+      [first, second | _] = row
+      # [145, 56, 200, 56, 145]
+      row ++ [second, first]
+   end
+
+   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b }}
+   end
+
+   def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+    %Identicon.Image{hex: hex}
+   end
+end
