389b416c247fce09a960e929dec8f5f42798f1ac
Alex Sahm <sahm.alexander@pwc.com>
2019-07-31 09:15:07 +0200

    finalized drawing identicon image

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,6 +5,37 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_sqares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+   end
+
+   def save_image(image, input) do
+      File.write("#{input}.png", image)
+
+   end
+
+   # In this case we don't need the image variable. Thats why we don't specify it in argument.
+   def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+      image = :egd.create(250, 250)
+      fill = :egd.color(color)
+      Enum.each pixel_map, fn({start, stop}) ->
+         :egd.filledRectangle(image, start, stop, fill)
+      end
+      :egd.render(image)
+   end
+
+   # Pattern matching can be done in argument list without passing the var itself, here '= image'
+   # In this case we need the image var. Thats why we specify it explicitly in argument.
+   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+      pixel_map = Enum.map grid, fn({_code, index}) ->
+         horizontal = rem(index, 5) * 50
+         vertical = div(index, 5) * 50
+         top_left = {horizontal, vertical}
+         bottom_right = {horizontal + 50, vertical + 50}
+         {top_left, bottom_right}
+      end
+      %Identicon.Image{image | pixel_map: pixel_map}
    end
 
    def filter_odd_sqares(%Identicon.Image{grid: grid} = image) do
